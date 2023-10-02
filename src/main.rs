a8e508df4ab3f2f3873b185bfd7b9f915dbdbd33
darcy <acednes@gmail.com>
2023-10-02 18:40:11 +1100

    get text from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use clap::Parser;
-
+use std::io;
 use title::{print_title, Args};
 
 fn main() {
     let args = Args::parse();
 
+    // Use given width, or get from terminal
     let width = args.width.unwrap_or_else(|| {
         term_size::dimensions()
             .expect("Failed to get terminal columns. Try running with `--width` argument")
             .0
     });
 
-    let text = args.text.join(" ");
+    // From argument
+    let mut text = args.text.join(" ");
 
-    print_title(&text, width, args.style);
+    // From stdin
+    if text.trim().is_empty() {
+        io::stdin()
+            .read_line(&mut text)
+            .expect("Failed to read line");
+    }
+
+    print_title(text.trim(), width, args.style);
 }
